4dd6158e1b08725059986b5d4309c6b8176afc45
Abhitesh Nandipati <116545078+n-abhitesh@users.noreply.github.com>
2024-04-08 10:13:39 +0530

    Delete Part2 directory

--- Part2/README.md
+++
@@ -1,2 +0,0 @@
-# Assignment1-PDF-Processing-Application
-Streamlit application that processes PDFs either via Nougat or PyPDF python libraries
--- Part2/pages/Architecture.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("<h1 style='text-align: center;'>Architecture Diagram</h1>", unsafe_allow_html=True)
-st.image("./Part2/architecture_diagram.png", use_column_width=True)
